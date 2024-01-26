608d86762d55477d333debac9525d8ec124f6c57
jiminKang <wlals1234560@gmail.com>
2024-01-26 19:14:35 +0900

    sass 를 적용하여 todoTemplate 만들기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
 
 function App(props) {
   return (
     <div>
-      Todo App을 만들자
+      <TodoTemplate>Todo App을 만들자!</TodoTemplate>
     </div>
   );
 }
