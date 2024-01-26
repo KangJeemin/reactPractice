48907e4efde529dbdf2b6aa35369c70fa47a5fe0
jiminKang <wlals1234560@gmail.com>
2024-01-26 18:57:27 +0900

    Todo App 초기 설정 완료

--- my-app/src/useClassnames.js
+++
@@ -1,5 +0,0 @@
-const { DiffieHellmanGroup } = require("crypto");
-
-const MyComponent = ({highlighted, theme}) => (
-    <div classNames={('MyComponent', { highlighted }, theme)}>Hello</div>
-)
---
+++ todo-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(props) {
+  return (
+    <div>
+      Todo App을 만들자
+    </div>
+  );
+}
+
+export default App;
