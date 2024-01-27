c5d5e318c5878e1e670fda228b01638c57ff3080
jiminKang <wlals1234560@gmail.com>
2024-01-28 01:33:41 +0900

    react-virtualized 를 사용하여 2500개의  listitem 스크롤 렌더링 최적화

--- todo-app/src/components/TodoListitem.js
+++ todo-app/src/components/TodoListitem.js
@@ -8,18 +8,20 @@ import cn from 'classnames';
 import './styles/TodoListitem.scss'
 
 
-function TodoListitem({todo,onRemove,onToggle}) {
+function TodoListitem({todo,onRemove,onToggle,style}) {
     const {text, checked,id } = todo;
     
     return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox',{ checked})} onClick={()=>{onToggle(id)}}>
-                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/> }
-                <div className="text">{text}</div>
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox',{ checked})} onClick={()=>{onToggle(id)}}>
+                    {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/> }
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={()=>onRemove(id)}>
+                    <MdRemoveCircleOutline/>    
+                </div>            
             </div>
-            <div className="remove" onClick={()=>onRemove(id)}>
-                <MdRemoveCircleOutline/>    
-            </div>            
         </div>
     );
 }
