e2277a08e96ef2c88a0715c36f4381224ddaa93f
jiminKang <wlals1234560@gmail.com>
2024-02-16 11:51:57 +0900

    connect 함수 내부에 익명 함수 형태로 선언하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import Counter from "../components/Counter";
 import { connect } from 'react-redux';
 import { increase,decrease } from "../modules/counter";
+import { bindActionCreators } from 'redux'
 
 const CounterContainer =({number, increase, decrease}) => {
     return (
@@ -8,21 +9,16 @@ const CounterContainer =({number, increase, decrease}) => {
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-    // 임시 함수
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease:() => {
-        dispatch(decrease());
-    },
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number:state.counter.number,
+    }),
+    dispatch => 
+        bindActionCreators(
+            {
+                increase,
+                decrease
+            },
+        dispatch
+        )
 )(CounterContainer)
-
