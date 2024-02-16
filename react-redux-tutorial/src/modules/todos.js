d6c43a98d68400d66741924d6e8e67e05810154a
jiminKang <wlals1234560@gmail.com>
2024-02-16 13:13:32 +0900

    todos 모듈 또한 redux-aciton 을 사용하여 리덕스 편하게 사용하기 적용

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,31 +1,40 @@
-
+import { createAction, handleAction } from 'redux-actions'
 // 액션 타입 정의하기
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 Todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
-export const changeInput= input => ({
-    type:CHANGE_INPUT,
-    input
-});
+
+export const changeInput = createAction(CHANGE_INPUT,input=>input);
+// export const changeInput= input => ({
+//     type:CHANGE_INPUT,
+//     input
+// });
 let id =3; // insert가 호출될 때 마다 1씩 더해집니다. (사전에 todo 객체 두 개를 미리 넣어 둘 것임)
-export const insert = text=> ({
-    type:INSERT,
-    todo:{
-        id:id++,
-        text,
-        done:false
-    }
-});
-export const toggle = id => ({
-    type:TOGGLE,
-    id
-})
-export const remove = id => ({
-    type:REMOVE,
-    id
-})
+export const insert= createAction(INSERT, text=>({
+    id: id++,
+    text,
+    done:false
+}));
+// export const insert = text=> ({
+//     type:INSERT,
+//     todo:{
+//         id:id++,
+//         text,
+//         done:false
+//     }
+// });
+export const toggle = createAction(TOGGLE,id=>id);
+// export const toggle = id => ({
+//     type:TOGGLE,
+//     id
+// })
+export const remove = createAction(REMOVE, id=>id);
+// export const remove = id => ({
+//     type:REMOVE,
+//     id
+// })
 
 const initialState= {
     input:'',
@@ -43,33 +52,52 @@ const initialState= {
     ]
 };
 
-function todos(state=initialState,action){
-    switch (action.type){
-        case CHANGE_INPUT:
-            return{
-                ...state,
-                input:action.input
-            };
-        case INSERT:
-            return{
-                ...state,
-                todos: state.todos.concat(action.todo)
-            }
-        case TOGGLE:
-            return{
-                ...state,
-                todos:state.todos.map(todo=>
-                    todo.id === action.id ? {...todo, done: !todo.done}:todo
-                    )
-            }
-        case REMOVE:
-            return{
-                ...state,
-                todos:state.todos.filter(todo=> todo.id !== action.id)
-            }
-        default:
-            return state;
-    }
-}
+const todos = handleAction(
+    {
+        [CHANGE_INPUT]:(state,action)=>({...state, input: action.payload}),
+        [INSERT]:(state,action)=>({
+            ...state,
+            todos:state.todos.concat(action.payload),
+        }),
+        [TOGGLE]:(state,action)=>({
+            ...state,
+            todos:state.todos.map(todo=>
+                todo.id === action.payload ? {... todo, done: !todo.done} : todo,),
+        }),
+        [REMOVE]:(state,action)=>({
+            ...state,
+            todos:state.todos.filter(todo=>todo.id !== action.payload),
+        }),
+    },
+    initialState,
+);
+// function todos(state=initialState,action){
+//     switch (action.type){
+//         case CHANGE_INPUT:
+//             return{
+//                 ...state,
+//                 input:action.input
+//             };
+//         case INSERT:
+//             return{
+//                 ...state,
+//                 todos: state.todos.concat(action.todo)
+//             }
+//         case TOGGLE:
+//             return{
+//                 ...state,
+//                 todos:state.todos.map(todo=>
+//                     todo.id === action.id ? {...todo, done: !todo.done}:todo
+//                     )
+//             }
+//         case REMOVE:
+//             return{
+//                 ...state,
+//                 todos:state.todos.filter(todo=> todo.id !== action.id)
+//             }
+//         default:
+//             return state;
+//     }
+// }
 
 export default todos;
